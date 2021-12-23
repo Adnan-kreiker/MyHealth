3f3e7930f9e8da699a32ddb7b3f5c26a824baf58
Adnan-kreiker <adnankreiker@hotmail.com>
2021-12-23 11:47:26 +0200

    removed lazyload from recipes component

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,10 @@
     <hero-section></hero-section>
     <lazy-features id="features"></lazy-features>
     <lazy-calorie-calculator id="calculator"></lazy-calorie-calculator>
-    <render-on-scroll :offset-y="100" height="700px">
-      <lazy-recipes id="recipes"></lazy-recipes>
-    </render-on-scroll>
+    <lazy-recipes id="recipes"></lazy-recipes>
   </div>
 </template>
 
 <script>
-import RenderOnScroll from 'nuxt-render-on-scroll'
-
-export default {
-  components: {
-    RenderOnScroll,
-  },
-}
+export default {}
 </script>
-Features
